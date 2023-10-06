0ffffb89e5f06ff90f49ed4a20e229b2b4456d09
Dr-Bytez <bear.lvvb@mail.ru>
2023-10-06 19:30:42 +0200

    alpha version 06.10.2023

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^58p42rp*oo5ws)ch+dw%xa(^j=+oj(anyo70a2mg59))(&6ye
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
